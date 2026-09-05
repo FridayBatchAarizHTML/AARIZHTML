57a1a7793564ba38b9e24f744b82436bd897511c
FridayBatchAarizHTML <aarizyafi102@gmail.com>
2026-09-05 15:57:19 +0600

    Add files via upload

---
+++ number.py
@@ -0,0 +1,11 @@
+num1 = 45
+num2 = 13
+
+print("number 1", num1)
+print("number 2", num2)
+print("Additiom :", num1+num2)
+print("difference :", num1-num2)
+print("Product :", num1*num2)
+print("Division :", num1/num2)
+print("Floor Division :", num1//num2)
+print("Modulus Operation :", num1%num2)
